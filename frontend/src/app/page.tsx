b7202dcf14da8e7e1991a16cdfc5de6fc5c7cb7a
Sankalp <sanklpkaushik21@gmail.com>
2024-10-20 21:49:42 +0530

    adding Prisma ORM to Server with MongoDB

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>HI</Button>
+    </div>
+  );
 };
 
 export default App;
