1bd2942e01ff373d0917480ffea7ced0c2178620
Sankalp <sanklpkaushik21@gmail.com>
2024-10-21 22:04:32 +0530

    addign Login Page with components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/base/Herosection";
 
 const App = () => {
   return (
     <div>
-      <Button>HI</Button>
+      <HeroSection />
     </div>
   );
 };
