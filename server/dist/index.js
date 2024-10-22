0275dfffa11f1367ae88489557f036924f21ea44
Sankalp <sanklpkaushik21@gmail.com>
2024-10-22 22:25:05 +0530

    adding ejs to server for mailing system

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 7000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(_dirname, "./views"));
 app.get("/", (req, res) => {
-    return res.send("SEVER working");
+    return res.render("Welcome");
 });
 app.listen(PORT, () => console.log(`Server is Running on ${PORT}`));
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Application, Response, Request } from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(_dirname, "./views"));
+
 app.get("/", (req: Request, res: Response): any => {
-  return res.send("SEVER working");
+  return res.render("Welcome");
 });
 
 app.listen(PORT, () => console.log(`Server is Running on ${PORT}`));
